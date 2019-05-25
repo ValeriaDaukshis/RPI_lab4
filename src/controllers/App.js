15bce81aaa06f982b43fbba59c91ab2da2e49b6c
Valeria <daukshislera2000@gmail.com>
2019-05-25 13:26:38 +0300

    i can find news

--- src/controllers/App.js
+++ src/controllers/App.js
@@ -4,16 +4,23 @@ import AppModel from '../models/AppModel';
 export default class Application {
   constructor() {
     this.state = {
-      url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=ceb1e4eee8704cc5aedb2b87fa3497f3',
-      // url: 'https://newsapi.org/v2/sources?apiKey=ceb1e4eee8704cc5aedb2b87fa3497f3',
+      url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=ceb1e4eee8704cc5aedb2b87fa3497f3&maxResults=15',
     };
   }
 
   async start() {
-    const model = new AppModel(this.state);
-    const data = await model.getAllData();
-    console.log(data);
+    const model = new AppModel(0);
+    let data = await model.getAllData(this.state);
     model.processNewsResources(data);
-    return data;
+
+    document.querySelector('#load-btn')
+      .addEventListener('click', () => {
+        model.processNewsResources(data);
+      });
+
+    document.querySelector('#search-btn')
+      .addEventListener('click', () => {
+        data = model.findSomeTopics();
+      });
   }
 }
--- src/models/AppModel.js
+++ src/models/AppModel.js
@@ -1,32 +1,65 @@
 export default class AppModel {
-  constructor(state) {
-    this.state = state;
+  constructor(loadedNews) {
+    this.loadedNews = loadedNews;
   }
 
-  getTitles(data) {
-    return data.articles.map(title2 => title2.title);
-  }
-
-
-  async getAllData() {
-    const { url } = this.state;
-
+  async getAllData(state) {
+    const { url } = state;
     const responce = await fetch(url);
     const data = responce.json();
-    // const newsTitles = AppModel.getTitles(data);
+    // this.maxNews = data.sources.length;
     return data;
   }
 
   async processNewsResources(data) {
-    // for (let i = 0; i < 3; i++) {
-    const content = document.createElement('div');
-    for (let i = 0; i < 3; i++) {
+    // const content = document.createElement('div');
+    const content = document.querySelector('#resources');
+    for (let i = this.loadedNews; i < this.loadedNews + 4; i++) {
       content.innerHTML += ` 
-                    <p>${data.articles[i].title}</p>
-                    <img src="${data.articles[i].urlToImage}">
-                    <a href="${data.articles[i].url}">read more...</a>
+                    <p align="center">${data.articles[i].title}</p>
+                    <img src="${data.articles[i].urlToImage}" width="600" height="400" align="center" alt = "picture"><br>
+                    <a href="${data.articles[i].url}" align="right">read more...</a>
                                                     `;
     }
-    document.body.appendChild(content);
+    // document.body.appendChild(content);
+    this.loadedNews += 4;
+    return data;
+  }
+
+  async Results(data) {
+    // const content = document.createElement('div');
+    const content = document.querySelector('#findResults');
+    for (let i = this.loadedNews; i < this.loadedNews + 4; i++) {
+      // if (i + 1 >= this.maxNews) break;
+
+      content.innerHTML += ` 
+                    <p align="center">${data.articles[i].title}</p>
+                    <img src="${data.articles[i].urlToImage}" width="600" height="400" align="center" alt = "picture"><br>
+                    <a href="${data.articles[i].url}" align="right">read more...</a>
+                                                    `;
+    }
+    // document.body.appendChild(content);
+    this.loadedNews += 4;
+    return data;
+  }
+
+  async findSomeTopics() {
+    document.querySelector('#resources').style.display = 'none';
+    console.log();
+    const topic = document.querySelector('#search-field').value;
+    this.state = `https://newsapi.org/v2/everything?q=${topic}&apiKey=ceb1e4eee8704cc5aedb2b87fa3497f3&maxResults=15`;
+
+    const request = new Request(this.state);
+    fetch(request)
+      .then(response => response.json())
+      .then((data) => {
+        this.loadedNews = 0;
+        this.Results(data);
+        document.querySelector('#load-btn')
+          .addEventListener('click', () => {
+            this.Results(data);
+          });
+        return data;
+      });
   }
 }
